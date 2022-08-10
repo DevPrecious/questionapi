897fb4d454f7f0c79489e6ddab8ccb179aab5af3
Precious Oladele <oladelep77@gmail.com>
2022-08-09 18:07:25 -0700

    UserController

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index() 
+    {
+        return response([
+            'user' => auth()->user()
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/user', [UserController::class, 'index'])->name('user');
 });
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
